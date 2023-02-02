4666a02a7495c1cf9c53a37cb15f5c901fa0b5c1
redd <107080537+redd-rl@users.noreply.github.com>
2023-02-02 20:18:13 +0100

    Update conditional.js

--- conditional.js
+++ conditional.js
@@ -1,7 +1,7 @@
 
   var currentDate = new Date();
   var currentYear = new Date().getFullYear();
-  var targetCountdownDay = new Date("Feb 14, " + currentYear + " 12:00:00 AM GMT+1 (CET)");
+  var targetCountdownDay = new Date("Feb 2, " + currentYear + " 12:00:00 AM GMT+1 (CET)");
   var scriptAdded = false;
   
   function checkDate() {
