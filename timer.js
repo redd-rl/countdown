9d9b05307c9382b8199c88d07ace3899a2254b51
redd <107080537+redd-rl@users.noreply.github.com>
2023-02-03 21:41:59 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,25 +1,21 @@
 // Set the date we're counting down to
         var currentYear = new Date().getFullYear();
-        var countDownDate = new Date("Feb 3, " + currentYear + " 12:00:00 PM GMT+1 (CET)").getTime();
+        var countDownDate = new Date("Feb 14, " + currentYear + " 12:00:00 AM GMT+1 (CET)").getTime();
         var now = new Date().getTime();
         if (countDownDate < now) {
-              var currentYear = new Date().getFullYear()+1;
+              var currentYear = new Date().getFullYear();
               var countDownDate = new Date("Feb 14, " + currentYear + " 12:00:00 AM GMT+1 (CET)").getTime();
         } else {
-              var currentYear = new Date().getFullYear();
+              var currentYear = new Date().getFullYear()+1;
               var countDownDate = new Date("Feb 14, " + currentYear + " 12:00:00 AM GMT+1 (CET)").getTime();
         }
 
         // Update the count down every 1 second
         var x = setInterval(function() {
-            countDownDate = new Date("Feb 14, " + currentYear + " 12:00:00 PM GMT+1 (CET)").getTime();
+            countDownDate = new Date("Feb 14, " + currentYear + " 12:00:00 AM GMT+1 (CET)").getTime();
              // Get today's date and time
             var now = new Date().getTime();
             var distance = countDownDate - now;
-            if (countDownDate < now) {
-              currentYear = new Date().getFullYear()+1;
-              countDownDate = new Date("Feb 14, " + currentYear + " 12:00:00 AM GMT+1 (CET)").getTime();
-            }
             // Calculate the number of days until the countdown date
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
